09f8cd03af527f59d009146a6289b227b1d397cb
Marco Lubian <marco.lubian@live.it>
2015-06-28 14:46:30 +0200

    Root Js sample

---
+++ root.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Marco on 28/06/2015.
+ */
+
+function Loader () {
+    this.pointer = 0;
+    this.stringer = null;
+
+    this.setup = function() {
+        this.stringer = new String("My String example");
+        this.pointer += 1;
+    }
+}
